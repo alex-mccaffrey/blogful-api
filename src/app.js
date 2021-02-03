d62a16d94a20ac679ac46b64c2a3eee2801a0e93
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-02-03 07:38:17 -0700

    added post request with tests

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const articlesRouter = require('./articles/articles-router')
 
 const app = express()
+
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
@@ -14,6 +16,8 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.use('/articles', articlesRouter)
 
 
 app.get('/', (req, res) => {
